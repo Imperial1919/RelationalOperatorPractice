f8724c9d030ce0bb577813c3344cf3321d3a6cb2
Bryce <ImperialEagle1998@gmail.com>
2020-09-22 16:16:20 -0500

    First edition of the file in question.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace RelationalOperator_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int c;
+            int d;
+
+
+            b = 20;
+            a = 50;
+            c = 50;
+            d = 75;
+            
+
+            Console.WriteLine(a != b);
+            Console.WriteLine(a == b);
+            if ( a > b)
+            {
+                Console.WriteLine("variable a is greater than variable b.");
+            }
+
+            if (a < b)
+            {
+                Console.WriteLine("variable b is greater than variable b.");
+            }
+
+            else
+            {
+                Console.WriteLine("Error.");
+            }
+
+
+            Console.WriteLine(a >= c);
+        }
+    }
+}
